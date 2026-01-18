9e5c64d471d842f79324bf1974469f6cab4e7dbf
Marek Langiewicz <marek.langiewicz@gmail.com>
2026-01-18 18:05:25 +0100

    fun

---
+++ myboard/worker.js
@@ -0,0 +1,80 @@
+// This service worker caches index.html, so that the page loads instantly and works offline.
+const index_html = `
+<!DOCTYPE html>
+<!-- Based on https://github.com/vimium/vimium.github.io/tree/master/new-tab/ -->
+<!-- NOTE: This HTML must be kept in sync between index.html and worker.js. -->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="data:,">
+    <meta name="color-scheme" content="light dark">
+    <style type="text/css" media="screen">
+      body {
+        background-color: #101a1a;
+        color: #2f508e;
+        zoom: 125%;
+      }
+      ul {
+        list-style-type: none;
+      }
+      code.keys {
+        color: #8ab4f8;
+        width: 2rem;
+        display: inline-block;
+      }
+    </style>
+    <title>My Board</title>
+  </head>
+
+  <body id="myboard">
+    <!--
+      In Chrome, empirically we must put some content in the body to avoid the browser delaying the
+      rendering of the page and the painting of the page's background color. The delay can be up to
+      1 second and makes the page appear sluggish, as if it's not yet loaded. This delay appears to
+      be a Chrome rendering pipeline optimization for the typical website.
+
+      For this, we use transparent blank 1x1 GIF. Some text would also suffice.
+    -->
+    <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+    <h1>My Board</h1>
+    <h3>Local Key Maps</h3>
+    (enter insert mode first)
+    <ul id="local-keymaps">
+      <li>TODO</li>
+    </ul>
+    <h3>Search Engines</h3>
+    <ul id="search-engines">
+      <li>TODO</li>
+    </ul>
+  </body>
+
+  <script>
+    async function registerServiceWorker() {
+      try {
+        await navigator.serviceWorker.register("./worker.js");
+      } catch (error) {
+        console.error("Worker registration failed", error);
+      }
+    }
+    registerServiceWorker();
+  </script>
+</html>
+`;
+
+self.addEventListener("fetch", (event) => {
+  // We could serve index.html from the cache using `await caches.match(request)`. However, this
+  // takes remarkably long in Chrome as of 2025-08-09, such that there is a 1 second delay before
+  // the page is shown and ready for keyboard input. This is not the case with Firefox: the catched
+  // page is found and returned instantly. As a workaround, we just return the hardcoded contents of
+  // index.html without checking the cache.
+  const url = new URL(event.request.url);
+  if (url.pathname === "/myboard/index.html" || url.pathname == "/myboard/") {
+    event.respondWith(
+      new Response(index_html, {
+        headers: { "Content-Type": "text/html; charset=utf-8" },
+      }),
+    );
+    return;
+  }
+  // Otherwise, let the browser handle the fetch request.
+});
